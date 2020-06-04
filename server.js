4c6eca447cd273b05ba69c56ee52886916ff758b
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-06-04 19:11:22 -0400

    requester handlebars page template

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-    });
-});
+    app.listen(PORT, function() {
+        // Log (server-side) when our server has started
+        console.log("Server listening on: http://localhost:" + PORT);
+      });
+
