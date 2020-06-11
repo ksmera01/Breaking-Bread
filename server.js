bd85495d685bc6cafbb03a7df1506e930e27e566
Esther N <estheroids@gmail.com>
2020-06-11 00:42:20 -0400

    grab user id for fulfill, claim, submit btns

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ require("./routes/inventory-api-routes.js")(app);
 
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync({}).then(function () {
   app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
